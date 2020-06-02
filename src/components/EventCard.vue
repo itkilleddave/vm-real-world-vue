f940986bc6cf8fe4e755755797ded6c191cf1f0e
Dave Longbottom <davelongbottom@gmail.com>
2020-06-02 20:04:30 +0930

    start of tut

---
+++ src/components/EventCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <h1></h1>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            something: 'sdssds'
+        }
+    },
+}
+</script>
+<style>
+
+</style>
