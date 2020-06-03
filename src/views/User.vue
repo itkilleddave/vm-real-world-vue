df24b55b4d99c48ca7f089875f389141ea85da9d
Dave Longbottom <davelongbottom@gmail.com>
2020-06-03 19:46:32 +0930

    route prop

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,3 +1,9 @@
 <template>
-    <h1>This is {{ $route.params.username }}'s page</h1>
+    <h1>This is {{ username }}'s page</h1>
 </template>
+
+<script>
+export default {
+    props: ["username"]
+}
+</script>
